2ee012345c3a66fd78516523d0be19fe8351285f
Cliff Gor <clierozigy@gmail.com>
2019-05-13 01:27:16 +0300

    styled the image object and refactored the style props

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -6,13 +6,16 @@ import CardSection from './CardSection'
 
 const AlbumDetail = ({ album }) => {
     const {title, artist, thumbnail_image } = album;
+    const { thumbnailStyle, headerContentStyle } = styles
  return(
      <Card>
          <CardSection>
          <View>
-             <Image source={{ uri: thumbnail_image }}/>
+             <Image 
+             style={thumbnailStyle}
+             source={{ uri: thumbnail_image }}/>
          </View>
-         <View style={styles.headerContentStyle}>
+         <View style={headerContentStyle}>
          <Text>{title}</Text>
          <Text>{artist}</Text>
          </View>
@@ -24,7 +27,11 @@ const AlbumDetail = ({ album }) => {
 const styles ={
     headerContentStyle:{
         flexDirection: 'column',
-        justifyContent: 'space-around'
+        justifyContent: 'space-around',
+    },
+    thumbnailStyle:{
+        height: 50,
+        width: 50
     }
 }
 export default AlbumDetail
