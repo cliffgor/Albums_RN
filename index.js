ed9dbc5fc423ea48251e37f5b8695823a68d4867
Cliff Gor <clierozigy@gmail.com>
2019-05-12 07:13:18 +0300

    Imported the header component to the index.js file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Import A library to help create a component
 import React from 'react';
 import { Text, AppRegistry } from 'react-native'
+import Header from './src/components/header';
 
 //  Create a component 
 
