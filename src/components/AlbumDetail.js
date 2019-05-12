f96635711832d5709a6b59b93470ba3e4f41bb57
Cliff Gor <clierozigy@gmail.com>
2019-05-13 02:06:08 +0300

    Imported the Button component

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
-import CardSection from './CardSection'
+import CardSection from './CardSection';
+import Button from './Button'
 
 
 const AlbumDetail = ({ album }) => {
@@ -30,6 +31,13 @@ const AlbumDetail = ({ album }) => {
              style={imageStyle}
              source={{ uri: image }} />
          </CardSection>
+
+
+         <CardSection>
+             <Button />
+         </CardSection>
+
+         
      </Card>
  )
 };
